002abe670fd72d36518442ed5206581246d0e03f
Moh. Naofal Nurul Huda <moh.naofal@gmail.com>
2022-12-28 12:55:14 +0700

    init mysql

--- config/config.go
+++ config/config.go
@@ -1,1 +1,23 @@
 package config
+
+import "github.com/mohnaofal/rest-go-gin/config/mysql"
+
+type Config struct {
+	mysqlDB mysql.MySQLConnection
+}
+
+func Init() *Config {
+	cfg := new(Config)
+
+	cfg.InitMySQLDB()
+
+	return cfg
+}
+
+func (c *Config) InitMySQLDB() {
+	c.mysqlDB = mysql.ConnectionDB()
+}
+
+func (c *Config) MySQLDB() mysql.MySQLConnection {
+	return c.mysqlDB
+}
