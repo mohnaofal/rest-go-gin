46571204047b4ccd75ae83360af95ae5912d31c7
Moh. Naofal Nurul Huda <moh.naofal@gmail.com>
2022-12-28 17:44:55 +0700

    enhance init redis

--- config/redis/redis.go
+++ config/redis/redis.go
@@ -1,16 +1,19 @@
 package redis
 
 import (
+	"fmt"
 	"os"
+	"strconv"
 
 	"github.com/go-redis/redis"
 )
 
 func InitRedis() *redis.Client {
+	db, _ := strconv.Atoi(os.Getenv("REDIS_DB"))
 	rdb := redis.NewClient(&redis.Options{
-		Addr:     os.Getenv("REDIS_HOST"),
-		Password: os.Getenv("REDIS_PASSWORD"), // no password set
-		DB:       0,                           // use default DB
+		Addr:     fmt.Sprintf("%v:%v", os.Getenv("REDIS_HOST"), os.Getenv("REDIS_PORT")),
+		Password: os.Getenv("REDIS_PASSWORD"),
+		DB:       db,
 	})
 
 	if err := rdb.Ping().Err(); err != nil {
